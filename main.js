529d471a80ed3c6c3c20fa287da4a34d7f73c3cf
Mateusz Domański <mateusz.domanski@decsoft.com.pl>
2023-02-20 14:53:16 +0100

    Code refactor
    - window name
    - scripts placements
    - formatting

--- main.js
+++ main.js
@@ -26,8 +26,35 @@ class Point {
     }
 
     /**
-     * @param {Point} To 
+     * @param {Point} To
      * @returns {number} Distance represented by a floating point number of pixels
      */
     Distance = To => Math.hypot(To.x - this.x, To.y - this.y)
+}
+
+function inputPoint(ax, ay, bx, by) {
+    document.querySelector("#pointA").x.baseVal.value = +ax + plain.clientWidth / 2
+    document.querySelector("#pointA").y.baseVal.value = +ay + plain.clientWidth / 2
+
+    document.querySelector("#pointB").x.baseVal.value = +bx + plain.clientWidth / 2
+    document.querySelector("#pointB").y.baseVal.value = +by + plain.clientWidth / 2
+    calculate()
+}
+
+function clickPoint(e) {
+    const point = e.shiftKey ? document.querySelector("#pointB") : document.querySelector("#pointA")
+    const inputX = e.shiftKey ? bx : ax
+    const inputY = e.shiftKey ? by : ay
+    point.x.baseVal.value = e.offsetX
+    point.y.baseVal.value = e.offsetY
+    inputX.value = e.offsetX - plain.clientWidth / 2
+    inputY.value = e.offsetY - plain.clientHeight / 2
+    calculate()
+}
+
+function calculate() {
+    const A = new Point(ax.value, ay.value)
+    const B = new Point(bx.value, by.value)
+    rotation.value = A.Rotation(B)
+    distance.value = A.Distance(B)
 }
